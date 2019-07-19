00956bfec26c19acbafaf3ae05378cd04089d5d8
williamslai <williams0427@gmail.com>
2019-07-19 12:01:20 +0800

    Update perform.py

--- perform.py
+++ perform.py
@@ -6,11 +6,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 driver = webdriver.Chrome() 
 driver.get("https://drive.google.com/drive/folders/15xFrdgBOtI3THxlTP38NWGfwGFi7N8QJ")
 driver.maximize_window()
-driver.find_element_by_id("identifierId").send_keys("williams0427@gmail.com")
+driver.find_element_by_id("identifierId").send_keys("****")
 driver.find_element_by_id("identifierNext").click()
 #driver.find_element_by_xpath("//*[@id="password"]/div[1]/div/span/div/span/span/span[2]").click()
 sleep(2)#先用sleep，後續再改用wait
-driver.find_element_by_css_selector(".whsOnd.zHQkBf").send_keys("Le7350586") 
+driver.find_element_by_css_selector(".whsOnd.zHQkBf").send_keys("****") 
 driver.find_element_by_id("passwordNext").click()
 sleep(5)#先用sleep，後續再改用wait
 #定位到要右击的元素
